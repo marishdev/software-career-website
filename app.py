06cb2d4080d4f78b101af73664bb83f925b0a43b
marishdev <141897476+marishdev@users.noreply.github.com>
2024-03-12 17:16:12 +0000

    added api endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
@@ -30,12 +30,14 @@ JOBS = [
 
 ]
 
-
-
-
 @app.route("/home")
 def namecall():
     return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
