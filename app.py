dcb12b92fecee307e458c3814d5a67578f8a10f8
marishdev <141897476+marishdev@users.noreply.github.com>
2024-03-12 12:10:12 +0000

    home page created

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'ui/ux developer',
+    'location': 'chennai, India',
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'front-end developer',
+    'location': 'remote',
+    'salary': 'Rs. 8,00,000'
+  },
+{
+  'id': 4,
+  'title': 'back-end developer',
+  'location': 'remote',
+  'salary': 'Rs. 14,00,000'
+}
+
+]
+
+
+
+
+@app.route("/home")
 def namecall():
-    return "Hi marishwaran!"
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
+
+
